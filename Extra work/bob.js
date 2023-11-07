73418ef8635248a2266b0c525bf3b9a74741e23a
andrewwidner <andrew.widner@colorado.edu>
2023-11-07 11:07:18 -0700

    WA11

---
+++ Extra work/bob.js
@@ -0,0 +1,15 @@
+function sqrt(num){
+    return num ** (1/2);
+}
+
+function thirdrt(num){
+    return num ** (1/3);
+}
+function ispositiveandeven(num) {
+    if (num >= 0 && num%2 == 1){
+return 1;
+    }
+    else {
+return 0;
+    }
+}
